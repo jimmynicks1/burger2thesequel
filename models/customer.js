daa0641d304b9b3ff806f041f4db187ce66e87d0
jimmynicks1 <Jamesnickele@gmail.com>
2018-05-24 11:07:49 -0500

    add

---
+++ models/customer.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", 
+    {
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      classMethods: {
+
+        associate: function(models) {
+          Customer.hasMany(models.Burger)
+        }
+      }
+    });
+
+  return Customer;
+};
